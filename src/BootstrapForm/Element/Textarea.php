f0995be3acc93fb35205d4bbe3fa759b2a972ecc
Piotrek Rybałtowski <piotrek@rybaltowski.pl>
2012-07-01 19:17:12 +0200

    Added textarea field type

---
+++ src/BootstrapForm/Element/Textarea.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: piotrek
+ * Date: 6/20/12
+ * Time: 11:55 PM
+ */
+
+namespace BootstrapForm\Element;
+
+/**
+ * Textarea field
+ *
+ * @author Piotr Rybałtowski <piotrek@rybaltowski.pl>
+ */
+class Textarea extends Element
+{
+    /**
+     * @var string
+     */
+    protected $tag = 'textarea';
+}
